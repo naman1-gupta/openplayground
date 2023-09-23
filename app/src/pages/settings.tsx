c236ed1f71c76978dcda1b13400d0deae6248841
Naman <naman@playment.in>
2023-09-23 16:42:12 +0530

    Fix typo

--- app/src/pages/settings.tsx
+++ app/src/pages/settings.tsx
@@ -389,7 +389,7 @@ export default function Settings() {
       
       setProviders({
         ...providers,
-        [providerName]: {O
+        [providerName]: {
           ...providers[providerName],
           apiKey
         }
